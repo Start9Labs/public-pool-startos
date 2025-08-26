183f0e15bfe947e9bd9f7e05cdcf6db11b23fb6b
Remco Ros <remcoros@live.nl>
2025-08-26 17:27:25 +0000

    update to latest Makefile/github actions

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,4 +1,10 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'public-pool',
@@ -7,7 +13,8 @@ export const manifest = setupManifest({
   wrapperRepo: 'https://github.com/remcoros/public-pool-startos',
   upstreamRepo: 'https://github.com/benjamin-wilson/public-pool',
   supportSite: 'https://github.com/benjamin-wilson/public-pool/issues',
-  docsUrl: 'https://github.com/remcoros/public-pool-startos/blob/main/instructions.md',
+  docsUrl:
+    'https://github.com/remcoros/public-pool-startos/blob/main/instructions.md',
   marketingSite: 'https://web.public-pool.io',
   donationUrl: 'https://web.public-pool.io',
   description: {
@@ -20,9 +27,12 @@ export const manifest = setupManifest({
       source: {
         dockerBuild: {},
       },
-    },
+      arch: architectures,
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: {
+    arch: architectures,
+  },
   alerts: {
     install: null,
     update: null,
