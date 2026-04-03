3733535551d4075f127ff406f76e606245b4ab9c
Shadowy Super Coder <musashidisciple@proton.me>
2026-04-03 11:45:04 -0600

    add NETWORK=mainnet to .env

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_2_5_4 } from './v0.2.5.4'
+import { v_0_2_5_5 } from './v0.2.5.5'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_2_5_4,
+  current: v_0_2_5_5,
   other: [],
 })
